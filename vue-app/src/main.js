253cc0a851cc85a92e47afb95f7550214b19603d
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <110688069+ana-pfeilsticker@users.noreply.github.com>
2024-07-07 15:39:51 -0300

    build: sobe apenas o back end para a branch

--- vue-app/src/main.js
+++
@@ -1,7 +0,0 @@
-// main.js
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './routes/index.js'
-
-createApp(App).use(router).mount('#app') // monta o app
--- vue-app/src/repositories/usuario/obterInformacoesPerfilUsuario.js
+++
@@ -1,8 +0,0 @@
-export async function getInfoUserProfile(matricula) {
-	return new Promise((resolve, reject) => {
-		fetch(`http://localhost:3000/usuario_perfil/${matricula}`)
-			.then((resposta) => resposta.json())
-			.then((dados) => resolve(dados.data))
-			.catch((erro) => reject(erro));
-	});
-}
