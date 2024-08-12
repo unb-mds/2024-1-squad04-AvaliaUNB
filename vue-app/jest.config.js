8dacfb14066f58c27026d7189bcd61b95c1dd776
Atyrson <atyrson10@Hotmail.com>
2024-08-12 14:21:29 -0300

    Update jest.config.js

--- vue-app/jest.config.js
+++ vue-app/jest.config.js
@@ -1,4 +1,9 @@
 // jest.config.js
 module.exports = {
-  testMatch: ["**/tests/**/**/*.spec.js"],
+    testMatch: ["**/tests/**/**/*.spec.js"],
+    transform: {
+        "^.+\\.vue$": "vue-jest",
+        "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    },
+    moduleFileExtensions: ["js", "jsx", "json", "vue"],
 };
